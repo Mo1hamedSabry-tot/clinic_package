ff51112991600196cda2fe9f5935b83af462de46
Mohamed Sabry <141114067+Mo1hamedSabry-tot@users.noreply.github.com>
2024-06-25 14:32:13 +0300

    add export on app layer

--- lib/src/clinic_bloc/lib/src/src.dart
+++ lib/src/clinic_bloc/lib/src/src.dart
@@ -5,7 +5,6 @@ export 'doctor/doctor_bloc.dart';
 export 'get_me/get_me_bloc.dart';
 export 'medical_history/medical_history_bloc.dart';
 export 'prescription/prescription_bloc.dart';
-export 'prescription1/prescription_bloc.dart';
 export 'schedule/schedule_bloc.dart';
 export 'search/search_bloc.dart';
 export 'update_personal_data/update_personal_data_bloc.dart';
